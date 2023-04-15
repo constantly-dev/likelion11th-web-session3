a36fa2848898d71ca9f4cb90e75dcbff923de00c
constantly-dev <rntqhdl10@naver.com>
2023-04-15 16:48:51 +0900

    session3

--- react/src/pages/Main.js
+++ react/src/pages/Main.js
@@ -1,7 +1,26 @@
-import "../Main.css";
+import '../Main.css';
+
+import HobbyList from './HobbyList';
+import Info from './Info';
+import SpecialList from './SpecialList';
+import DarkMode from './DarkMode';
+
+import Data from './Data.json';
+
+import { useState } from 'react';
 
 const Main = () => {
-  return <></>;
+  const [isDark, setIsDark] = useState(true);
+  return (
+    <>
+      <div className={isDark ? 'darkMode' : ''}>
+        <DarkMode darkMode={setIsDark} dark={isDark}></DarkMode>
+        <Info data={Data} dark={isDark} />
+        <HobbyList data={Data} dark={isDark} />
+        <SpecialList data={Data} dark={isDark} />
+      </div>
+    </>
+  );
 };
 
 export default Main;
